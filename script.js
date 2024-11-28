d610e33abc640efc16d96ae286989ff97bdff8f1
muskalike <owen.c.hill@gmail.com>
2024-11-27 21:35:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const canvasHeight = canvas.height;
 let ballX = canvasWidth / 2;
 let ballY = canvasHeight / 2;
 let ballRadius = 8;
-let ballSpeedX = 1;
-let ballSpeedY = 1;
+let ballSpeedX = 1.5;
+let ballSpeedY = 1.5;
 
 // Player paddle properties
 const paddleWidth = 10;
